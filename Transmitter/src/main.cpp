ee3269709246c3b199c8cf4f6da8c491b299a8d0
Venz Burgos <venz.burgos@yahoo.com>
2024-03-09 01:27:37 -0800

    Transmitter Code Done

--- Transmitter/src/main.cpp
+++ Transmitter/src/main.cpp
@@ -1,12 +1,12 @@
-// #include <SPI.h>
-// #include <RH_RF95.h>
+#include <SPI.h>
+#include <RH_RF95.h>
 #include <Arduino.h>
 
 // Radio Communication Resources
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 3
-// RH_RF95 rf95(RFM95_CS, RFM95_INT);
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 // Button Pins Variables
 const int joyXPin = A0;
@@ -15,6 +15,10 @@ const int lockPos = 13;
 const int forward = 12;
 const int backward = 11;
 const int extraBtn = 10;
+const int powerLED = 6;
+
+// Latency
+const int latency = 10;
 
 // True state means unlocked, continue sending values
 // False state means locked, position is stuck
@@ -22,13 +26,6 @@ int lockState = 1;
 int forwState = 1;
 int backState = 1;
 int xtraState = 1;
-
-// // Track prev states for debouncing
-// int lockPrevState = 1;
-// int forwPrevState = 1;
-// int backPrevState = 1;
-// int xtraPrevState = 1;
-// const unsigned long debounceInterval = 50;
 
 // String Initialize
 String joyX = "";
@@ -55,19 +52,20 @@ void setup() {
   Serial.begin(9600);
 
   // Radio Initializing
-  // Serial.println("RFM95 Transmitter Initializing...");
-  // while(!rf95.init()) {
-  //   rf95.init();
-  //   Serial.println("RFM95 initialization failed!");
-  //   delay(1000);
-  // }
-  // Serial.println("RFM95 Transmitter Initialized");
+  Serial.println("RFM95 Transmitter Initializing...");
+  while(!rf95.init()) {
+    rf95.init();
+    Serial.println("RFM95 initialization failed!");
+    delay(1000);
+  }
+  Serial.println("RFM95 Transmitter Initialized");
 
   // Setup Pins and Interrupts
   pinMode(lockPos, INPUT_PULLUP);
   pinMode(forward, INPUT_PULLUP);
   pinMode(backward, INPUT_PULLUP);
   pinMode(extraBtn, INPUT_PULLUP);
+  pinMode(powerLED, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(lockPos), interruptLock, CHANGE);
   attachInterrupt(digitalPinToInterrupt(forward), interruptForward, CHANGE);
   attachInterrupt(digitalPinToInterrupt(backward), interruptBackward, CHANGE);
@@ -78,63 +76,22 @@ void setup() {
   forwState = digitalRead(forward);
   backState = digitalRead(backward);
   xtraState = digitalRead(extraBtn);
-
+  digitalWrite(powerLED, HIGH);
 }
 
 void loop() {
   if(digitalRead(lockPos)){
-    // joyX = String(analogRead(joyXPin));
-    // joyY = String(analogRead(joyYPin));
-    // XAndY = joyX + "," + joyY;
-    // rf95.send((uint8_t*)XAndY.c_str(), XAndY.length());
-    // rf95.waitPacketSent();
-    joyX = String(random(1000));
-    joyY = String(random(1000));
+    joyX = String(analogRead(joyXPin));
+    joyY = String(analogRead(joyYPin));
     XAndY = joyX + "," + joyY;
   }
 
-  // totalData = "LOCK:" + String(digitalRead(lockPos))+ " FORW:" +String(digitalRead(forward))+ " BACK:" +String(digitalRead(backward))+ " XTRA:" + String(digitalRead(extraBtn)) + " " + XAndY;
-  totalData = "LOCK:" + String(lockState)+ " FORW:" +String(forwState)+ " BACK:" +String(backState)+ " XTRA:" + String(xtraState) + " " + XAndY;
+  totalData = "L" + String(lockState)+ "F" +String(forwState)+ "B" +String(backState)+ "X" + String(xtraState) + "P" + XAndY;
 
-  Serial.println(totalData);
-  delay(100);
+  rf95.send((uint8_t*)totalData.c_str(), totalData.length());
+  rf95.waitPacketSent();
+
+  // Fix if battery dies too quick, polling rate of analog data
+  delay(latency);
+  // Serial.println(totalData);
 }
-
-
-
-// const int batteryPin = A5; // Analog pin connected to BAT pin
-// const int usbPin = 6;     // Digital pin connected to USB power input
-
-// void setup() {
-//   Serial.begin(9600);
-//   pinMode(usbPin, INPUT_PULLDOWN);
-// }
-
-// float getBatteryVoltage() {
-//   // Read the analog value and convert it to voltage
-//   float voltage = analogRead(batteryPin) * (3.3 / 1023.0);
-//   return voltage;
-// }
-
-// float getBatteryPercentage(float voltage, float minVoltage = 3.0, float maxVoltage = 4.2) {
-//   // Calculate battery percentage based on voltage range
-//   float percentage = ((voltage - minVoltage) / (maxVoltage - minVoltage)) * 100.0;
-//   return constrain(percentage, 0, 100); // Ensure percentage is within 0-100 range
-// }
-
-// void loop() {
-//   float batteryVoltage = getBatteryVoltage();
-//   float batteryPercentage = getBatteryPercentage(batteryVoltage);
-//   bool charging = digitalRead(usbPin) == HIGH;
-
-//   Serial.print("Battery Voltage: ");
-//   Serial.print(batteryVoltage);
-//   Serial.print("V, ");
-//   Serial.print("Battery Percentage: ");
-//   Serial.print(batteryPercentage);
-//   Serial.print("%, ");
-//   Serial.print("Charging: ");
-//   Serial.println(charging ? "Yes" : "No");
-
-//   delay(1000); // Adjust delay as needed
-// }
